eb8d878779e3c1ba38cc5f48b92379b2e0d7449e
Emily Morgan <you@example.com>
2020-10-16 21:28:58 -0400

    adding what we did in class

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(() => {
+    // make an AJAX request using FETCH API
+    fetch('./data/classData.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            //handleData
+            // here's where you would call the function that puts the pieces on the page 
+        })
+    .catch((err) => {
+        console.log(err);
+    })
+
+})();
