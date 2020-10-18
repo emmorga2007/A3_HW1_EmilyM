9bb4b4dfea5661e0120db29587b282ed8a849a6c
Emily Morgan <you@example.com>
2020-10-17 20:46:04 -0400

    homework finished

--- js/main.js
+++ js/main.js
@@ -4,12 +4,31 @@
         .then(res => res.json())
         .then(data => {
             console.log(data);
-
+            
+            courseContent(data)
             //handleData
             // here's where you would call the function that puts the pieces on the page 
         })
     .catch((err) => {
-        console.log(err);
+        handleFetchError()
     })
+
+    function handleFetchError() {
+    courseCode = document.getElementById("courseCode")
+    teachName = document.getElementById("teachName")
+    courseTimes = document.getElementById("courseTimes")
+    courseCode.innerHTML = "Fetch Failed"
+    teachName.innerHTML = "Fetch Failed"
+    courseTimes.innerHTML = "Fetch Failed"
+    }
+    
+    function courseContent(data) {
+    courseCode = document.getElementById("courseCode")
+    teachName = document.getElementById("teachName")
+    courseTimes = document.getElementById("courseTimes")
+    courseCode.innerHTML = data["coursename"] + "-" + data["coursecode"]
+    teachName.innerHTML = "Professor - " + data["profname"]
+    courseTimes.innerHTML = data["classtime"][0] + " - " + data["classtime"][1]
+    }
 
 })();
